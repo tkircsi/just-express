df836c435f4ef0a73d1e3f8315a56aea31d479e5
Tibor Kircsi <tkircsi@gmail.com>
2020-04-02 19:53:36 +0200

    login done

--- express_3/loginSite.js
+++ express_3/loginSite.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 
 const helmet = require('helmet');
 app.use(helmet());
@@ -8,6 +9,7 @@ app.use(helmet());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser('cookie-secret-sign'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +21,28 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
   res.render('login');
 });
+
+app.post('/process_login', (req, res) => {
+  const { username, password } = req.body;
+  if (password === '123') {
+    res.cookie('username', username, { httpOnly: true });
+    res.redirect('/welcome');
+  } else {
+    res.redirect('/login?msg=fail');
+  }
+});
+
+app.get('/welcome', (req, res) => {
+  res.render('welcome', {
+    username: req.cookies.username
+  });
+});
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/login');
+});
+
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
 });
